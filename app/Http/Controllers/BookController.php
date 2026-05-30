b588b4f59999ba0bc2030c39d1ef399f8803fa37
atikahadzman <atikah.adzman@gmail.com>
2026-05-30 17:44:28 +0800

    for some reason try/catch suddenly not working

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -23,43 +23,44 @@ class BookController extends Controller
 
     public function store(Request $request)
     {
-        try {
-            $validated = $request->validate([
-                'title' => 'required|string|max:255',
-                'cover_image' => 'required|image|mimes:jpeg,png,jpg,webp|max:2048',
-                'author' => 'required|string|max:255',
-                'description' => 'required',
-                'book_url' => 'required|file|mimes:pdf|max:10240',
-                'total_pages' => 'required|integer',
-            ]);
+        $validated = $request->validate([
+            'title' => 'required|string|max:255',
+            'cover_image' => 'required|image|mimes:jpeg,png,jpg,webp|max:2048',
+            'author' => 'required|string|max:255',
+            'description' => 'required',
+            'book_url' => 'required|file|mimes:pdf|max:10240',
+            'total_pages' => 'required|integer',
+        ]);
 
-            $book = Book::create([
-                'title' => $request->title,
-                'author' => $request->author,
-                'description' => $request->description,
-                'total_pages' => $request->total_pages,
-                'added_by' => $request->user()->id,
-            ]);
+        $book = Book::create([
+            'title' => $request->title,
+            'author' => $request->author,
+            'description' => $request->description,
+            'total_pages' => $request->total_pages,
+            'added_by' => $request->user()->id,
+        ]);
 
-            if ($request->hasFile('cover_image')) {
-                $book->addMediaFromRequest('cover_image')
-                    ->toMediaCollection('cover_image');
-            }
+        if ($request->hasFile('cover_image')) {
+            $book->addMediaFromRequest('cover_image')
+                ->toMediaCollection('cover_image');
+        }
 
-            if ($request->hasFile('book_url')) {
-                $book->addMediaFromRequest('book_url')
-                    ->toMediaCollection('book_url');
-            }
+        if ($request->hasFile('book_url')) {
+            $book->addMediaFromRequest('book_url')
+                ->toMediaCollection('book_url');
+        }
 
+        if (!$book) {
             return response()->json([
-                'status' => 'success',
-                'data' => $book
-            ], 201);
-        } catch (Throwable $e) {
-            return response()->json([
+                'status' => 'error',
                 'message' => $e->getMessage(),
             ], 500);
         }
+
+        return response()->json([
+            'status' => 'success',
+            'data' => $book
+        ], 201);
     }
 
     public function show(string $id)
